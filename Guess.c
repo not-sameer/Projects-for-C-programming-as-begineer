8f5e1ab8e0174cd6863c990bb5baf78bba285ddb
not-sameer <sameer.082bei034@tcioe.edu.np>
2025-12-25 23:16:24 +0545

    Add files via upload

---
+++ Guess.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+#include<time.h>
+int main()
+{
+    int guessed;
+int no_of_guess=0;
+srand(time(0));
+int randomnumber=rand()%100 +1;
+do {
+printf("Please guess your number\t");
+scanf("%d",&guessed);
+if (guessed>randomnumber)
+{
+printf("You guessed higher, lower it\n");
+}
+else if(guessed<randomnumber){
+    printf("You guessed lower, increase it\n");
+}
+else
+{
+    goto yep;
+}
+no_of_guess++;
+
+}while (guessed!=randomnumber);
+yep:
+printf("Congrats!!! You guessed the number in %d times.",no_of_guess +1);
+return 0;
+}
